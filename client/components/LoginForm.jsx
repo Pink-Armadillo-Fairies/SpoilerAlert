71acc630efd66106d59c4e4c8d2b3ed543ef481f
Kayla <krspecie@gmail.com>
2024-07-13 18:21:16 -0400

    complete login page for visual. still need to work through managing state

--- client/components/LoginForm.jsx
+++ client/components/LoginForm.jsx
@@ -1,22 +1,33 @@
 import React from 'react';
 import { useState } from 'react';
-import Form from 'react-bootstrap/Form';
+import { Form, Container, Button } from 'react-bootstrap';
 import '../../client/styles.css';
 
 
 
 const LoginForm= () => {
     return(
-        <Form>
-          <Form.Group className="emailInput" controlId="emailInput">
-            <Form.Label>Email Address</Form.Label>
-            <Form.Control type="email"/>
-          </Form.Group>
-          <Form.Group className="passwordInput" controlId="passwordInput">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" />
+     <Container className="loginFormContainer">
+      
+      <Form className="loginBox">
+      <p style={{fontFamily: "arial", fontSize: "20px"}}>Login</p>
+        <Form.Group className="usernameInput" controlId="usernameInput" style={{display: "flex", flexDirection: "column", alignItems: "start"}}>
+          <Form.Label style={{fontFamily: "arial", color: "black", fontSize: "10px"}}>Username</Form.Label>
+          <Form.Control type="email" value={"TBD"} onChange={"TBD"}/>
+        </Form.Group>
+          <Form.Group className="passwordInput" controlId="passwordInput" style={{display: "flex", flexDirection: "column", alignItems: "start"}}>
+            <Form.Label style={{fontFamily: "arial", color: "black", fontSize: "10px"}}>Password</Form.Label>
+            <Form.Control type="password" value={"TBD"} onChange={"TBD"}/>
           </Form.Group> 
+          <Button className="loginButton" variant="login" type="submit" onPress={"TBD"}>Log In</Button>
+          <div className="newUserLine" style={{display: "flex",  alignItems: "center", fontFamily: "Arial", fontSize: "10px", marginTop: "40px"}}>
+            <p>New to Spoiler Alert?</p>
+            <a href="/create-user" style={{marginLeft: "10px"}}>Create New User</a>
+          </div>
+          
         </Form>
+        
+    </Container>
     )
 }
 
