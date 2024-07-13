79a45ae5fd09419679032f232d79dbf8dee396f8
Mickey Capper <mickey.capper@gmail.com>
2024-07-13 13:32:20 -0600

    connecting database

--- server/server.js
+++ server/server.js
@@ -2,12 +2,18 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 const path = require('path');
+const pgp = require('pg-promise');
+const db = pgp(
+  'postgresql://postgres.gjitkjeyoojbanjtglag:touchy-withdrew-wear@aws-0-us-east-1.pooler.supabase.com:6543/postgres'
+);
 
-app.use(express.static(path.join(__dirname, '../build')))
+app.use(express.static(path.join(__dirname, '../build')));
 
-app.get('/', (req,res)=> res.sendFile(path.join(__dirname, '../build/index.html'))); // Serve from the current directory
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '../build/index.html'))
+); // Serve from the current directory
 
-
+app.use('*', (req, res) => res.sendStatus(404));
 
 // Global error handler
 app.use((err, req, res, next) => {
