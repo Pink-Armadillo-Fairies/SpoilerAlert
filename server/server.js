2582ba0bdd7e7b094c48f442b70bf89c141671a9
Gavin <gshadinger1@gmail.com>
2024-07-13 11:32:18 -0700

    express render err

--- server/server.js
+++ server/server.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
-//const path = require('path');
+const path = require('path');
 
-app.use(express.static(__dirname)); // Serve from the current directory
-// Route for serving the index.html file
-// app.get('/', (req, res) => {
-//   res.sendFile(__dirname + '');
-// });
+app.get('/', express.static(path.join(__dirname, '../build'))); // Serve from the current directory
+
+// Global error handler
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: 'Express error handler caught unknown error',
+    status: 500,
+    message: { err: 'An error occurred' },
+  };
+  const errorObj = Object.assign({}, defaultErr, err);
+  console.log(errorObj.log);
+  return res.status(errorObj.status).json(errorObj.message);
+});
 
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
