50d56c65b7071cbd30edee24b9b9f351a9d059e2
Kayla <krspecie@gmail.com>
2024-07-15 18:50:57 -0400

    started to experiment with show selection page

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import LoginForm from './LoginForm.jsx';
+import ShowSelection from './ShowSelection.jsx';
+import { Route, Routes } from 'react-router-dom';
 
 const App = () => {
   return (
     <>
           <h1>Spoiler Alert!</h1>
-          <LoginForm />
-
+          <Routes>
+            <Route path='/' element={<LoginForm />} />
+            <Route path='/shows' element={<ShowSelection />} />
+          </Routes>
     </>
   )
 }
--- client/main.js
+++ client/main.js
@@ -2,10 +2,13 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './components/App.jsx';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 const container = document.getElementById('app');
 const root = createRoot(container);
 
 root.render(
-  <App />
+  <Router>
+    <App />
+  </Router>
 );
