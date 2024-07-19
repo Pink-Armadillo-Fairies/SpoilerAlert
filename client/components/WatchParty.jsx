8eadeec1ee8d075f58829de71273d86f904ce072
Mickey Capper <mickey.capper@gmail.com>
2024-07-18 23:39:16 -0600

    handling some edge cases when duplicate views are logged and making variant a property of views

--- client/components/WatchParty.jsx
+++ client/components/WatchParty.jsx
@@ -13,37 +13,46 @@ const WatchParty = () => {
   const me = useSelector((store) => store.login.username);
   let header = <></>;
 
+  
   stateParty.forEach((el) => {
     //create a value to index viewer's progress in a series
-    const viewOrder = el.season * 1000 + el.episode;
+    let viewOrder = el.season * 1000 + el.episode;
+    if(el.user===me){viewOrder=viewOrder+0.5}
     //keep a record of the latest episode the viewer has watched
     maxCache[el.user] === undefined
-      ? (maxCache[el.user] = viewOrder)
-      : (maxCache[el.user] = Math.max(maxCache[el.user], viewOrder));
+    ? (maxCache[el.user] = viewOrder)
+    : (maxCache[el.user] = Math.max(maxCache[el.user], viewOrder));
+    
+    
     //add the view to the party array along with the viewOrder value
     party.push({ ...el, viewOrder });
   });
-
+  
   party.sort((a, b) => a.viewOrder - b.viewOrder);
-
+  
   const relativeStatus = (el) => {
     let variant = '';
-    el.user === me
-      ? (variant = 'primary')
-      : el.viewOrder > maxCache[me]
-      ? (variant = 'danger')
-      : (variant = 'success');
-
+    
     return variant
   };
+  
+  party.forEach((el) => {
+    el.user === me
+      ? (el.variant = 'primary')
+      : el.viewOrder > maxCache[me]
+      ? (el.variant = 'danger')
+      : (el.variant = 'success');
+    
+  })
 
   party.forEach((el) => {
     if (el.viewOrder === maxCache[el.user]) {
       watchList.push(
-        <ListGroup.Item variant = {relativeStatus(el)}>
+        <ListGroup.Item variant = {el.variant}>
           {`${el.user} - watched up to S${el.season}, Ep ${el.episode}`}
         </ListGroup.Item>
       );
+      maxCache[el.user]='posted'
     }
   });
 
