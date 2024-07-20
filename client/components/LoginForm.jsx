026c930037de75d7523529f774241172e24f854a
Ryan Gilbert <ryanbgilbert@gmail.com>
2024-07-20 11:34:15 -0700

    Add login failed message

--- client/components/LoginForm.jsx
+++ client/components/LoginForm.jsx
@@ -2,102 +2,123 @@ import React from 'react';
 import { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-import { successfulLogin } from '../loginSlice';
-import { Form, Container, Button } from 'react-bootstrap';
+import { successfulLogin, failedLogin } from '../loginSlice';
+import { Form, Container, Button, Alert } from 'react-bootstrap';
 import '../../client/styles.css';
 
+const LoginForm = () => {
+  const [loginInput, updateLoginEntered] = useState({
+    usernameInput: '',
+    passwordInput: '',
+  });
 
+  const handleInputChange = (event) => {
+    const { id, value } = event.target;
+    updateLoginEntered({
+      ...loginInput,
+      [id]: value,
+    });
+  };
 
+  const dispatch = useDispatch();
 
-const LoginForm= () => {
-    // const [inputUsernameValue, setInputUsernameValue] = useState('');
-    // const [inputPasswordValue, setInputPasswordValue] = useState('');
+  const navigate = useNavigate();
 
-    const [loginInput, updateLoginEntered] = useState({
-      usernameInput: '',
-      passwordInput: '',
-    })
-
-    const handleInputChange = (event) => {
-      // console.log('event', event)
-      const { id, value } = event.target;
-      updateLoginEntered({
-        ...loginInput,
-        [id]: value,
-      })
-      // console.log('loginInput', loginInput);
-    }
-
-    const dispatch = useDispatch();
-    // const navigate = useNavigate();
-
-    // const handleUsernameChange = (event) => {
-    //   setInputUsernameValue(event.target.value);
-    // }
-    // const handlePasswordChange = (event) => {
-    //   setInputPasswordValue(event.target.value);
-    // }
-
-    const handleLoginSubmit = () => {
-      alert(`Username: ${inputUsernameValue} Password: ${inputPasswordValue}`)
-    }
-
-    const navigate = useNavigate();
-  //   const gotoMainPage = () => {
-  //   navigate('/mainpage');
-  // };
-
-    const handleSubmit = async (event) => {
-      event.preventDefault();
-      try {
-        //console.log('fetching', loginInput.user);
-        const response = await fetch('/users/login', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await fetch('/users/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
         },
-          body: JSON.stringify(loginInput),
-        });
-        if (response.ok) {
-          // const responseData = await response.json();
-          // console.log('verifyUser Response:', responseData);
-          dispatch(successfulLogin(loginInput.usernameInput))
-          navigate('/mainpage');
-        } else {
-          console.error('Login failed');
-        }
-      } catch (err) {
-        console.error('Error with login fetch:', err)
+        body: JSON.stringify(loginInput),
+      });
+      if (response.ok) {
+        dispatch(successfulLogin(loginInput.usernameInput));
+        navigate('/mainpage');
+      } else {
+        dispatch(failedLogin(loginInput.usernameInput));
+        console.error('Login failed');
       }
+    } catch (err) {
+      console.error('Error with login fetch:', err);
     }
+  };
 
-    return(
+  const loginMessage = useSelector((store) => store.login.loginFailedMessage);
+  let alert = <></>;
+  if (loginMessage) {
+    alert = (
+      <Alert style={{ margin: '8px' }} variant="danger">
+        {loginMessage}
+      </Alert>
+    );
+  }
 
-
-     <Container className="loginFormContainer">
-
-
-      <Form className="loginBox" onSubmit={handleSubmit} >
-
-      <p style={{fontFamily: "arial", fontSize: "20px"}}>Login</p>
-        <Form.Group className="usernameInput" controlId="usernameInput" style={{display: "flex", flexDirection: "column", alignItems: "start"}}>
-          <Form.Label style={{fontFamily: "arial", color: "black", fontSize: "10px"}}>Username</Form.Label>
-          <Form.Control type="text" onChange={handleInputChange}/>
+  return (
+    <Container className="loginFormContainer">
+      <Form className="loginBox" onSubmit={handleSubmit}>
+        <p style={{ fontFamily: 'arial', fontSize: '20px' }}>Login</p>
+        <Form.Group
+          className="usernameInput"
+          controlId="usernameInput"
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'start',
+          }}
+        >
+          <Form.Label
+            style={{ fontFamily: 'arial', color: 'black', fontSize: '10px' }}
+          >
+            Username
+          </Form.Label>
+          <Form.Control type="text" onChange={handleInputChange} />
         </Form.Group>
-          <Form.Group className="passwordInput" controlId="passwordInput" style={{display: "flex", flexDirection: "column", alignItems: "start"}}>
-            <Form.Label style={{fontFamily: "arial", color: "black", fontSize: "10px"}}>Password</Form.Label>
-            <Form.Control type="password"  onChange={handleInputChange} />
-          </Form.Group> 
-          <Button className="loginButton" variant="login" type="submit" style={{border: "1px solid black"}} >Log In</Button>
-          <div className="newUserLine" style={{display: "flex", alignItems: "center", fontFamily: "Arial", fontSize: "10px", marginTop: "40px"}}>
-            <p style={{margin:"0"}}>New to Spoiler Alert?</p>
-            <a href="/create-user" style={{marginLeft: "20px"}}>Create New User</a>
-          </div>
-          
-        </Form>
-        
+        <Form.Group
+          className="passwordInput"
+          controlId="passwordInput"
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'start',
+          }}
+        >
+          <Form.Label
+            style={{ fontFamily: 'arial', color: 'black', fontSize: '10px' }}
+          >
+            Password
+          </Form.Label>
+          <Form.Control type="password" onChange={handleInputChange} />
+        </Form.Group>
+        <Button
+          className="loginButton"
+          variant="login"
+          type="submit"
+          style={{ border: '1px solid black' }}
+        >
+          Log In
+        </Button>
+        {alert}
+        <div
+          className="newUserLine"
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            fontFamily: 'Arial',
+            fontSize: '10px',
+            marginTop: '40px',
+          }}
+        >
+          <p style={{ margin: '0' }}>New to Spoiler Alert?</p>
+          <a href="/create-user" style={{ marginLeft: '20px' }}>
+            Create New User
+          </a>
+        </div>
+      </Form>
     </Container>
-    )
-}
+  );
+};
 
 export default LoginForm;
--- client/loginSlice.js
+++ client/loginSlice.js
@@ -1,26 +1,26 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-
-const initialState =  {
-    username: '',
-    loginStatus: false,
-}
+const initialState = {
+  username: '',
+  loginStatus: false,
+};
 
 const loginSlice = createSlice({
-    name: 'login',
-    initialState,
-    reducers: {
-        //log in successful
-        successfulLogin: (state, action) => {
-            //console.log(state)
-            state.loginStatus = true;
-            state.username =action.payload;
-            console.log("state:", state);
-            //console.log("state.loginStatus:", state.loginStatus); 
-            //console.log("username", state.userName)
-        }
-    }
-})
-export const { successfulLogin } = loginSlice.actions;
+  name: 'login',
+  initialState,
+  reducers: {
+    //log in successful
+    successfulLogin: (state, action) => {
+      state.loginStatus = true;
+      state.username = action.payload;
+      console.log('state:', state);
+    },
+    failedLogin: (state, action) => {
+      state.loginStatus = false;
+      state.loginFailedMessage = 'Login failed. Try again';
+      console.log('state:', state);
+    },
+  },
+});
+export const { successfulLogin, failedLogin } = loginSlice.actions;
 export default loginSlice.reducer;
-
