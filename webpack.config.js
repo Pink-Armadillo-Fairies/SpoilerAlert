7eae06ea5cab2979d798547058f7e11a41f47f6d
shundayo131 <shundayo.131@gmail.com>
2024-07-23 17:31:01 -0700

    updated script in package.json and webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     path: path.resolve(__dirname, 'build'),
     publicPath: '/',
   },
-  mode: 'development',
+  mode: process.env.NODE_ENV,
   module: {
     rules: [
       {
