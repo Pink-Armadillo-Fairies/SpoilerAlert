45a9c6d5c2f7f08275bafef4698dea408e48b8b4
shundayo131 <shundayo.131@gmail.com>
2024-07-24 17:44:24 -0700

    updated db config

--- server/db_config.js
+++ server/db_config.js
@@ -2,7 +2,11 @@ require('dotenv').config();
 const pgp = require('pg-promise')(/* options */);
 
 const db = pgp(
-  `postgresql://postgres.gjitkjeyoojbanjtglag:${process.env.SUPABASE_PG_KEY}@aws-0-us-east-1.pooler.supabase.com:6543/postgres`
+  // use this URL for original database 
+  // `postgresql://postgres.gjitkjeyoojbanjtglag:${process.env.SUPABASE_PG_KEY}@aws-0-us-east-1.pooler.supabase.com:6543/postgres` 
+
+  // use this URL for new database 
+  `postgresql://postgres.imtqizjjrbybmdgufnwd:${process.env.SUPABASE_PG_KEY}@aws-0-us-west-1.pooler.supabase.com:6543/postgres`
 );
 
 module.exports = db;
