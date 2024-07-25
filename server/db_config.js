61892538fb939996d116d324d7c6c04232934048
Chris <chrisnash819@yahoo.com>
2024-07-24 20:00:12 -0700

    search show end point, createShow, and searchShow middleware

--- server/db_config.js
+++ server/db_config.js
@@ -3,7 +3,7 @@ const pgp = require('pg-promise')(/* options */);
 
 const db = pgp(
   // use this URL for original database 
-  // `postgresql://postgres.gjitkjeyoojbanjtglag:${process.env.SUPABASE_PG_KEY}@aws-0-us-east-1.pooler.supabase.com:6543/postgres` 
+  //`postgresql://postgres.gjitkjeyoojbanjtglag:${process.env.SUPABASE_PG_KEY}@aws-0-us-east-1.pooler.supabase.com:6543/postgres` 
 
   // use this URL for new database 
   `postgresql://postgres.imtqizjjrbybmdgufnwd:${process.env.SUPABASE_PG_KEY}@aws-0-us-west-1.pooler.supabase.com:6543/postgres`
--- server/showController.js
+++ server/showController.js
@@ -36,9 +36,13 @@ const show = {
 
   createShow: async (req, res, next) => {
     try {
-      const title = req.body.title;
+      // const {title, photoURL, } = res.locals.show
+      //const title = res.locals.show.name;
+      //Column name in DB is 'title', response from API is 'name'
+      const {name, image} = res.locals.show;
+      console.log(name, image.medium);
       const result = await db.none(
-        `INSERT INTO shows (title) VALUES ('${title}')`
+        `INSERT INTO shows (title, image) VALUES ('${name}', '${image.medium}' )`
       );
       return next();
     } catch (err) {
@@ -49,6 +53,28 @@ const show = {
       return next(errObj);
     }
   },
+
+  searchShows: async (req, res, next) => {
+    try {
+      // TO-DO: update search to be the value we receive from the search input
+      const searchInput = 'breaking bad';
+      const response = await fetch(`https://api.tvmaze.com/singlesearch/shows?q=${searchInput}`);
+      console.log('response', response);
+
+      let data = await response.json();
+
+      res.locals.show = data;
+    
+      next();      
+
+    } catch (err) {
+      const errObj = {
+        log: `search show failed: ${err}`,
+        message: { err: 'search show failed, check server log for details' },
+      };
+      return next(errObj);
+    }
+  }
 };
 
 module.exports = show;
