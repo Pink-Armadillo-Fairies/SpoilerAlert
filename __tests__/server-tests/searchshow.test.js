5d9a624006f38deff8ef4e28c7c0439ce05e3890
Chris <chrisnash819@yahoo.com>
2024-08-01 19:48:56 -0700

    re-organized testing

--- __tests__/server-tests/searchshow.test.js
+++ __tests__/server-tests/searchshow.test.js
@@ -10,11 +10,4 @@ describe('GET /searchshows', () => {
     });
   });
 
-  describe('GET /getcomments', () => {
-    it('should return array of comment objects, for passed in showId', async () => {
-        const showId = 133;
-        const response = await request(app).get(`/getcomments?showId=${showId}`);
-      expect(response.statusCode).toBe(200);
-      expect(response.body[0].show_id).toBe('133');
-    });
-  });
+
