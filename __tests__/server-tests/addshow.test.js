f0bc869179ca499846ee31be3db07420dfce8472
Chris <chrisnash819@yahoo.com>
2024-08-01 19:44:31 -0700

    testing suites

---
+++ __tests__/server-tests/addshow.test.js
@@ -0,0 +1,20 @@
+const request = require('supertest');
+const app = require('../../server/server.js');
+
+describe('GET /searchshows', () => {
+    it('should return show object', async () => {
+        const searchQuery = 'Breaking Bad';
+        const response = await request(app).get(`/searchshows?searchQuery=${searchQuery}`);
+      expect(response.statusCode).toBe(200);
+      expect(response.body.name).toBe('Breaking Bad');
+    });
+  });
+
+  describe('GET /getcomments', () => {
+    it('should return array of comment objects, for passed in showId', async () => {
+        const showId = 133;
+        const response = await request(app).get(`/getcomments?showId=${showId}`);
+      expect(response.statusCode).toBe(200);
+      expect(response.body[0].show_id).toBe('133');
+    });
+  });
